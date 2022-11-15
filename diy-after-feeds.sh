6e5bf0a984fe548e24f916ec8e24690b23e3d320
KevinMX <mx@kevinmx.top>
2022-11-15 14:53:00 +0800

    diy/after-feeds: replace cn ip cidr source
    The china_ssr.txt file from vssr is way to large to fit in some routers, and I have no idea where it came from. Replacing it with clang.cn's IP list for now, since it's also the default setting in vssr's LuCI WebUI.

--- diy-after-feeds.sh
+++ diy-after-feeds.sh
@@ -8,3 +8,7 @@ sed -i 's/OpenWrt/Railgun/g' package/base-files/files/bin/config_generate
 
 #Fix mosdns build (see https://github.com/QiuSimons/openwrt-mos/issues/126)
 rm -rf feeds/packages/net/mosdns
+
+#Replace vssr cn_ip_cidr source
+rm -f package/small-package/luci-app-vssr/root/etc/vssr/china_ssr.txt
+curl -o package/small-package/luci-app-vssr/root/etc/vssr/china_ssr.txt https://ispip.clang.cn/all_cn_cidr.txt
