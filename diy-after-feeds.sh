a79fd2fdd4a2aab6aa1b457b5a0eb13995302ad5
KevinMX <mx@kevinmx.top>
2022-12-04 23:45:56 +0800

    [experimental]diy/after-feeds: fix build error

--- diy-after-feeds.sh
+++ diy-after-feeds.sh
@@ -15,3 +15,12 @@ curl -o package/small-package/luci-app-vssr/root/etc/vssr/china_ssr.txt https://
 
 #Fix NAT Loopback
 sed -i "/exit 0/i\echo 'net.bridge.bridge-nf-call-arptables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-ip6tables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-iptables=0' >> /etc/sysctl.conf" package/lean/default-settings/files/zzz-default-settings
+
+#Fix build error
+wget https://github.com/coolsnowwolf/lede/commit/75d227e14fd9bec5f0748403351e3e86ba678440.patch
+wget https://github.com/coolsnowwolf/lede/commit/62461731d77cb739238449777d42711cf8d9478a.patch
+wget https://github.com/coolsnowwolf/packages/commit/770c5159f8be36f626eb2344cb223b614141f8bc.patch
+git apply -R 62461731d77cb739238449777d42711cf8d9478a.patch
+git apply -R 75d227e14fd9bec5f0748403351e3e86ba678440.patch
+cd feeds/packages/net
+git apply -R ../../../770c5159f8be36f626eb2344cb223b614141f8bc.patch
