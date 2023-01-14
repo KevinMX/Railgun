f2424386f7ee85611e3c6a737cf23058c42ad29e
KevinMX <mx@kevinmx.top>
2023-01-14 16:17:51 +0800

    diy/newifi_d2: clean up dependencies

--- diy/ramips_mt7621/newifi_d2/diy-after-feeds.sh
+++ diy/ramips_mt7621/newifi_d2/diy-after-feeds.sh
@@ -5,3 +5,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # Modify default hostname
 sed -i 's/ImmortalWrt/Railgun/g' package/base-files/files/bin/config_generate
+
+# Clean up dependencies for helloworld master
+rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy,redsocks2,sagernet-core,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan,v2ray-core,v2ray-geodata,v2ray-plugin,v2raya,xray-core,xray-plugin}
+rm -rf feeds/packages/lang/lua-neturl
+rm -rf feeds/luci/applications/luci-app-ssr-plus
--- diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
+++ diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
@@ -4,8 +4,5 @@
 #rm -rf feeds/packages/net/mosdns
 git clone --depth=1 https://github.com/QiuSimons/openwrt-mos package/openwrt-mos
 
-#Use master branch of helloworld and clean up dependencies
-rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy,redsocks2,sagernet-core,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan,v2ray-core,v2ray-geodata,v2ray-plugin,v2raya,xray-core,xray-plugin}
-rm -rf feeds/packages/lang/lua-neturl
-rm -rf feeds/luci/applications/luci-app-ssr-plus
+#Use master branch of helloworld
 git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
