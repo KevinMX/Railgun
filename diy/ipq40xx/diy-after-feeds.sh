3caa6e520633dd8841ee889697bd8bb5c8a28f51
KevinMX <mx@kevinmx.top>
2023-01-19 16:28:03 +0800

    diy: use lean's mosdns a.k.a. sbwml/luci-app-mosdns by default

--- diy/ipq40xx/diy-after-feeds.sh
+++ diy/ipq40xx/diy-after-feeds.sh
@@ -10,6 +10,9 @@ sed -i 's/OpenWrt/Railgun/g' package/base-files/files/bin/config_generate
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
 
-#Replace vssr cn_ip_cidr source
+# Replace vssr cn_ip_cidr source
 rm -f package/small-package/luci-app-vssr/root/etc/vssr/china_ssr.txt
 curl -o package/small-package/luci-app-vssr/root/etc/vssr/china_ssr.txt https://ispip.clang.cn/all_cn_cidr.txt
+
+# Remove breaking mosdns v5 support, revert back to sbwml/luci-app-mosdns
+rm -rf package/small-package/{mosdns,luci-app-mosdns}
