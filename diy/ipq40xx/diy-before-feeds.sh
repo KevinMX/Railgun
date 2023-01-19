ab1381f7f00b7738e7ff13599710a30dd97b39cc
KevinMX <mx@kevinmx.top>
2023-01-19 16:39:17 +0800

    diy: fix homebox svn export path

--- diy/ipq40xx/diy-before-feeds.sh
+++ diy/ipq40xx/diy-before-feeds.sh
@@ -3,4 +3,4 @@
 git clone --depth=1 https://github.com/kenzok8/small-package package/small-package
 
 # Add hoomebox dependency for luci-app-netspeedtest
-svn export https://github.com/sirpdboy/netspeedtest/trunk/homebox
+svn export https://github.com/sirpdboy/netspeedtest/trunk/homebox package/small-package/homebox
