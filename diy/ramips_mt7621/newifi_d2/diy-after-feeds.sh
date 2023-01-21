cfa1196f90dd1942789e33a2718e7ba6c004d359
KevinMX <mx@kevinmx.top>
2023-01-22 00:39:11 +0800

    diy/mt7621: fix script path

--- diy/ramips_mt7621/newifi_d2/diy-after-feeds.sh
+++ diy/ramips_mt7621/newifi_d2/diy-after-feeds.sh
@@ -12,4 +12,4 @@ rm -rf feeds/packages/lang/lua-neturl
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 
 # Fix NAT Loopback
-sed -i "/exit 0/i\echo 'net.bridge.bridge-nf-call-arptables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-ip6tables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-iptables=0' >> /etc/sysctl.conf" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\echo 'net.bridge.bridge-nf-call-arptables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-ip6tables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-iptables=0' >> /etc/sysctl.conf" package/emortal/default-settings/files/99-default-settings
--- diy/ramips_mt7621/phicomm_k2p/diy-after-feeds.sh
+++ diy/ramips_mt7621/phicomm_k2p/diy-after-feeds.sh
@@ -15,4 +15,4 @@ echo 'WNMEnable=1' >> package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
 echo 'WNMEnable=1' >> package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
 
 # Fix NAT Loopback
-sed -i "/exit 0/i\echo 'net.bridge.bridge-nf-call-arptables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-ip6tables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-iptables=0' >> /etc/sysctl.conf" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\echo 'net.bridge.bridge-nf-call-arptables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-ip6tables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-iptables=0' >> /etc/sysctl.conf" package/emortal/default-settings/files/99-default-settings
