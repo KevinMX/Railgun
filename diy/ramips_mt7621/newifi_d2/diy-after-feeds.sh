cf13e1def990b9ce83d5135937b0b8efff21777a
KevinMX <mx@kevinmx.top>
2023-01-23 16:21:49 +0800

    newifi_d2: fix mosdns LuCI support

--- diy/ramips_mt7621/newifi_d2/diy-after-feeds.sh
+++ diy/ramips_mt7621/newifi_d2/diy-after-feeds.sh
@@ -13,3 +13,8 @@ rm -rf feeds/luci/applications/luci-app-ssr-plus
 
 # Fix NAT Loopback
 sed -i "/exit 0/i\echo 'net.bridge.bridge-nf-call-arptables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-ip6tables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-iptables=0' >> /etc/sysctl.conf" package/emortal/default-settings/files/99-default-settings
+
+# Clean up dependencies for mosdns master
+rm -rf $(find . -name mosdns) $(find . -name v2ray-geodata)
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
--- diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
+++ diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Use master branch of mosdns and add LuCI support
-#rm -rf feeds/packages/net/mosdns
-git clone --depth=1 https://github.com/QiuSimons/openwrt-mos package/openwrt-mos
-
 #Use master branch of helloworld
 git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
