45a6c653784592555fb1aedaa113df24ce1b3890
KevinMX <mx@kevinmx.top>
2023-01-26 19:16:02 +0800

    Revert: [temporary] seeds: add homebox package for luci-app-netspeedtest
    homebox is already added in small-package.

--- diy/x86_64/diy-before-feeds.sh
+++ diy/x86_64/diy-before-feeds.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Add packages from kenzok8/small-package
 git clone --depth=1 https://github.com/kenzok8/small-package package/small-package
-
-# Add hoomebox dependency for luci-app-netspeedtest
-svn export https://github.com/sirpdboy/netspeedtest/trunk/homebox package/small-package/homebox
