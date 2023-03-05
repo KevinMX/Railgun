7ba411aea30922ae7204c54f076910f5151b5b58
KevinMX <mx@kevinmx.top>
2023-03-05 21:30:28 +0800

    Revert: x86_64: add luci-app-design-config
    Already added in small-package.

--- diy/x86_64/diy-before-feeds.sh
+++ diy/x86_64/diy-before-feeds.sh
@@ -2,5 +2,3 @@
 # Add packages from kenzok8/small-package
 git clone --depth=1 https://github.com/kenzok8/small-package package/small-package
 
-# Add luci-theme-design configuration plugin
-git clone --depth=1 https://github.com/gngpp/luci-app-design-config package/luci-app-design-config
