92e303e6aef6a3f31474ed439f3db03f4280b91d
KevinMX <mx@kevinmx.top>
2023-03-14 15:59:31 +0800

    seed/newifi_d2: add luci-app-passwall

--- diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
+++ diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-#Use master branch of helloworld
+# Use master branch of helloworld
 git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
+
+# Use master branch of passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwall
+
