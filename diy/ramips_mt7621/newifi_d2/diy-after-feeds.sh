1b8c55e129d00e860ebf8ddb639c38dd516cec0f
KevinMX <mx@kevinmx.top>
2023-04-18 20:48:35 +0800

    diy/ramips: remove NAT loopback workarounds
    https://github.com/padavanonly/immortalwrt/commit/f3a2093ab9b9d907507241ba9f9e247129e4aaea#diff-187fe82c9c81c7c64967fb1d23b9f5b792d5168a63c6c47998ce29f3ba4738b2R13

--- diy/ramips_mt7621/newifi_d2/diy-after-feeds.sh
+++ diy/ramips_mt7621/newifi_d2/diy-after-feeds.sh
@@ -11,9 +11,6 @@ rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,micr
 rm -rf feeds/packages/lang/lua-neturl
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 
-# Fix NAT Loopback
-sed -i "/exit 0/i\echo 'net.bridge.bridge-nf-call-arptables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-ip6tables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-iptables=0' >> /etc/sysctl.conf" package/emortal/default-settings/files/99-default-settings
-
 # Clean up dependencies for mosdns master
 rm -rf $(find . -name mosdns) $(find . -name v2ray-geodata)
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/mosdns
--- diy/ramips_mt7621/phicomm_k2p/diy-after-feeds.sh
+++ diy/ramips_mt7621/phicomm_k2p/diy-after-feeds.sh
@@ -9,6 +9,3 @@ sed -i 's/ImmortalWrt/Railgun/g' package/base-files/files/bin/config_generate
 # Enable 802.11 r
 sed -i 's/FtSupport=0/FtSupport=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
 sed -i 's/FtSupport=0/FtSupport=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
-
-# Fix NAT Loopback
-sed -i "/exit 0/i\echo 'net.bridge.bridge-nf-call-arptables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-ip6tables=0' >> /etc/sysctl.conf\necho 'net.bridge.bridge-nf-call-iptables=0' >> /etc/sysctl.conf" package/emortal/default-settings/files/99-default-settings
