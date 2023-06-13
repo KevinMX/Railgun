010afe86aedac72fd4d4a1833cf754f5c64d3b10
KevinMX <mx@kevinmx.top>
2023-06-13 12:19:59 +0800

    diy/ramips: deprecate svn export
    https://github.blog/2023-01-20-sunsetting-subversion-support/

--- diy/ramips_mt7621/phicomm_k2p/diy-before-feeds.sh
+++ diy/ramips_mt7621/phicomm_k2p/diy-before-feeds.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+function git_sparse_clone() {
+    git clone --filter=blob:none --no-checkout --depth=1 $1 && cd $2
+    git sparse-checkout init --cone
+    git sparse-checkout set $3
+    git checkout
+    mv $3 ../$4
+    cd ../ && rm -rf $2
+}
+
 # Add mtkiappd support for 802.11 k/v/r
-svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/mt/mtkiappd package/kernel/mt-drivers/mtkiappd
+git_sparse_clone https://github.com/coolsnowwolf/lede lede package/lean/mt/mtkiappd package/kernel/mt-drivers/mtkiappd
