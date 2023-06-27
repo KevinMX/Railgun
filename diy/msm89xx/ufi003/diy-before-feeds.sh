dbc678ec8b39dff0af8dff0a79c92f999f0e9c75
Kevin.MX <mx@kevinmx.top>
2023-06-28 01:23:24 +0800

    diy/ufi003: fix typo

--- diy/msm89xx/ufi003/diy-before-feeds.sh
+++ diy/msm89xx/ufi003/diy-before-feeds.sh
@@ -5,4 +5,4 @@ git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # Add luci-app-smstool
-git clone --depth-1 https://github.com/4IceG/luci-app-sms-tool package/luci-app-sms-tool
+git clone --depth=1 https://github.com/4IceG/luci-app-sms-tool package/luci-app-sms-tool
