50338fc6029399f176f8b4b3aa1537f4ff5549de
KevinMX <mx@kevinmx.top>
2024-01-26 12:24:17 +0800

    diy/before: add missing feeds

--- diy/x86_64/diy-before-feeds.sh
+++ diy/x86_64/diy-before-feeds.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# Enable helloworld
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add passwall
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default    
