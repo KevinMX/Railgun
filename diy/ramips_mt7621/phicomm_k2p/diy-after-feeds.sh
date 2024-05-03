0a2421ee15cee9635c50d5a493715afab9180d34
archon111 <15860797585@163.com>
2024-05-03 18:47:56 +0800

    Update diy-after-feeds.sh

--- diy/ramips_mt7621/phicomm_k2p/diy-after-feeds.sh
+++ diy/ramips_mt7621/phicomm_k2p/diy-after-feeds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Modify Default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify default hostname
 sed -i 's/OpenWrt/Railgun/g' package/base-files/files/bin/config_generate
