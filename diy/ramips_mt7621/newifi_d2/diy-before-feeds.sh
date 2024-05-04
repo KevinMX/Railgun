6ff4e29d4c11393779f76a5f6a191bdcf8ca0f62
KevinMX <mx@kevinmx.top>
2024-05-05 03:42:24 +0800

    newifi_d2: fix builds on newer distros

--- diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
+++ diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
@@ -6,3 +6,5 @@ git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
 # Use master branch of passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwall
 
+# Patch include/prereq-build.mk for building on distros with newer python versions
+patch -p1 < files/00_python3.patch
