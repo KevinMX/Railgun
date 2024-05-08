01dd3de64bbe0101f2ea1b838a000be17ed462ec
archon111 <15860797585@163.com>
2024-05-08 11:25:12 +0800

    Update diy-before-feeds.sh
    add netbird

--- diy/ramips_mt7621/phicomm_k2p/diy-before-feeds.sh
+++ diy/ramips_mt7621/phicomm_k2p/diy-before-feeds.sh
@@ -14,4 +14,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add passwall
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default    
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git netbird https://github.com/messense/openwrt-netbird.git;main' >> feeds.conf.default
