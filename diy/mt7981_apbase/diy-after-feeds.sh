72f66a235654eb7e21eebd765c411a82f64d06dd
KevinMX <mx@kevinmx.top>
2025-10-29 00:07:49 +0800

    mt7981_apbase: enable legacy 11b/g
    Signed-off-by: KevinMX <mx@kevinmx.top>

--- diy/mt7981_apbase/diy-after-feeds.sh
+++ diy/mt7981_apbase/diy-after-feeds.sh
@@ -17,3 +17,6 @@ sed -i 's/192.168.6.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default Hostname
 sed -i 's/ImmortalWrt/Railgun/g' package/base-files/files/bin/config_generate
+
+# Enable legacy 802.11b/g (experimental)
+git apply 01_enable_11bg.patch
